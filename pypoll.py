f666d6f03f1be3444d6b3e342866fb1459631100
Natasha-Nelson <81983110+Natasha-Nelson@users.noreply.github.com>
2021-05-02 18:37:54 -0400

    Add files via upload

--- pypoll.py
+++ pypoll.py
@@ -15,6 +15,11 @@ candidate_list = []
 #create a Dictionary for Vote Counts
 candidate_votes = {}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -36,7 +41,25 @@ with open(file_to_load) as election_data:
         #start adding votes for each candidate
         candidate_votes[candidate_name] += 1
 
-print(candidate_votes)
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    # divide the value of vote for candidate by total votes
+    vote_percentage = float(votes) / float(total_Votes) * 100
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_percentage = vote_percentage
+        winning_count = votes
+        winning_candidate = candidate_name
+        
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 
 #The Types of Data We Need To Get
